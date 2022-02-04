32f662eac96bb4e1c015392fdbce49aeceabfcb9
Nicolas balao <nicolasbalao2@gmail.com>
2022-02-04 17:44:14 +0100

    Page destination responsive do

---
+++ space-tourism-website-main/js/header.js
@@ -0,0 +1,31 @@
+document.write(`<div class="logo">
+<img src="/assets/shared/logo.svg" alt="logo" />
+</div>
+<div class="menu-hamburger-group">
+<div class="hamburger">
+  <img src="/assets/shared/icon-hamburger.svg" alt="hamburger" />
+</div>
+<nav class="menu hidden">
+  <img
+    class="close-menu"
+    src="assets/shared/icon-close.svg"
+    alt="close slide menu"
+  />
+
+  <ul>
+    <li class="active">
+      <a href="#"> <span>00</span> Home</a>
+    </li>
+    <li>
+      <a href="#"><span>01</span> Destination</a>
+    </li>
+    <li>
+      <a href="#"><span>02</span> Crew</a>
+    </li>
+    <li>
+      <a href="#"><span>03</span> Technology</a>
+    </li>
+  </ul>
+</nav>
+</div>
+`);
